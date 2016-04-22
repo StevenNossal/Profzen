9a0cb676c9774a9ef3bc6e0775e91d1e3cfdc77b
Steven Nossal <steven_nossal@fairfaxcollegiate.com>
2016-04-22 12:15:57 -0400

    Stub versions of profzen server lws protocol handlers

---
+++ server/profzen-annotator.c
@@ -0,0 +1,19 @@
+
+/* profzen-annotator.c */
+
+#include "profzen-server.h"
+
+int
+callback_profzen_annotator(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len)
+{
+	struct per_session_data__profzen_annotator *pss = 
+		(struct per_session_data__profzen_annotator *) user;
+
+	switch (reason) {
+
+		default:
+			break;
+		}
+
+	return 0;
+}
---
+++ server/profzen-writer.c
@@ -0,0 +1,18 @@
+/* profzen-writer.c */
+
+#include "profzen-server.h"
+
+int
+callback_profzen_writer(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len)
+{
+	struct per_session_data__profzen_writer *pss = 
+		(struct per_session_data__profzen_writer *) user;
+
+	switch (reason) {
+
+		default:
+			break;
+		}
+
+	return 0;
+}
